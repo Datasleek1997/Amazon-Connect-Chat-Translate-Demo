cd2394c24ff7ff223ca6c4e2e9c480d94bd55edc
Datasleek1997 <131283802+Datasleek1997@users.noreply.github.com>
2024-11-21 17:22:05 +0530

    Update Deposition.js 21NOV

--- src/components/Deposition.js
+++ src/components/Deposition.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./chatroom.css";
 function Deposition() {
   const [loading, setLoading] = useState(false);
-   const [isActive, setIsActive] = useState(true);
+  const [isActive, setIsActive] = useState(true);
   const Depositionurl =
     "https://t86a6l5lk4.execute-api.us-east-1.amazonaws.com/production";
   const DepositionHandleChnage = async (e) => {
@@ -52,28 +52,24 @@ function Deposition() {
     const myValue = localStorage.getItem("myKey");
     if (myValue) {
       //post method
-     
-    
-    const response = await fetch(resume, {
-      method: "POST",
 
-      body: JSON.stringify({
-        contactid: myValue,
-      }),
-    });
-    const data = await response.json();
-    if (data.body === "True") {
-      
-      alert("Recording resume");
-          setIsActive(true)
+      const response = await fetch(resume, {
+        method: "POST",
 
-    } else {
-      alert("Recording not resume");
-          setIsActive(false)
-
+        body: JSON.stringify({
+          contactid: myValue,
+        }),
+      });
+      const data = await response.json();
+      if (data.body === "True") {
+        alert("Recording resume");
+        setIsActive(true);
+      } else {
+        alert("Recording not resume");
+        setIsActive(false);
+      }
+      console.log(data);
     }
-    console.log(data);
-  }
   };
   const pausebuttonfnc = async (e) => {
     console.log("pause button clicked");
@@ -82,36 +78,30 @@ function Deposition() {
     const myValue = localStorage.getItem("myKey");
     if (myValue) {
       //post method
-    
-    
-    const response = await fetch(pauseapi, {
-      method: "POST",
 
-      body: JSON.stringify(
-        {
+      const response = await fetch(pauseapi, {
+        method: "POST",
+
+        body: JSON.stringify({
           contactid: myValue,
-        }
-      ),
-    });
-    const data = await response.json();
-    if (data.body === "True") {
-          setIsActive(false)
+        }),
+      });
+      const data = await response.json();
+      if (data.body === "True") {
+        setIsActive(false);
 
-      alert("Recording paused");
-    } else {
-      alert("Recording not paused");
-          setIsActive(true)
+        alert("Recording paused");
+      } else {
+        alert("Recording not paused");
+        setIsActive(true);
+      }
 
-      
+      console.log(data);
     }
-    
-    console.log(data);
   };
-}
   return (
-    <>
-      <div className="DepositionContainer">
-       <div style={{"height":"70%"}}>
+    <div className="DepositionContainer">
+      <div style={{ height: "70%" }}>
         <div className="Depositionheader">
           <p>Disposition</p>
         </div>
@@ -119,87 +109,249 @@ function Deposition() {
           <div className="DepositionCode">Loading...</div>
         ) : (
           <div className="DepositionCode">
-            <button value="Cancel" onClick={(e) => DepositionHandleChnage(e)}>
-              Cancel
+            <button
+              value="Genral-Modification question"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Genral-Modification question
             </button>
             <button
-              value="Change or Modification"
+              value="General-Policy question"
               onClick={(e) => DepositionHandleChnage(e)}
             >
-              Change or Modification
+              General-Policy question
             </button>
             <button
-              value="Pre-booking"
+              value="General-Hotel confirmation Number"
               onClick={(e) => DepositionHandleChnage(e)}
             >
-              Pre-booking
+              General-Hotel confirmation Number
             </button>
             <button
-              value="Escalation During Travel"
+              value="General-confirming booking"
               onClick={(e) => DepositionHandleChnage(e)}
             >
-              Escalation During Travel
+              General-confirming booking
             </button>
             <button
-              value="Complaint Post Travel"
+              value="Genral-Resend itinerary"
               onClick={(e) => DepositionHandleChnage(e)}
             >
-              Complaint Post Travel
+              General-Resend itinerary
             </button>
             <button
-              value="General Question"
+              value="General - Special Request"
               onClick={(e) => DepositionHandleChnage(e)}
             >
-              General Question
+              General - Special Request
             </button>
-                
-            <button value="Irop" onClick={(e) => DepositionHandleChnage(e)}>
-              Irop
-            </button>
-            <button value="Others" onClick={(e) => DepositionHandleChnage(e)}>
-              Others
-            </button>
-                <button
-              value="General inquiry to Air"
+            <button
+              value=" General-update contact information"
               onClick={(e) => DepositionHandleChnage(e)}
             >
-             General inquiry to Air
+              General-update contact information
             </button>
-                                <button
-              value="Requesting to change Air"
+            <button
+              value="Cancel-In penalty"
               onClick={(e) => DepositionHandleChnage(e)}
             >
-       Requesting to change Air
+              Cancel-In penalty
             </button>
-                                <button
-              value="Requesting to cancel Air
-"
+            <button
+              value="Cancel-Not in penalty"
               onClick={(e) => DepositionHandleChnage(e)}
             >
-            Requesting to cancel Air
-
+              Cancel-Not in penalty
             </button>
+            <button
+              value="Cancel-Waiver request"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Cancel-Waiver request
+            </button>
+            <button
+              value="Pre booking-Shopping assistance"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Pre booking-Shopping assistance
+            </button>
+            <button
+              value="Pre booking-Points query"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Pre booking-Points query
+            </button>
+            <button
+              value="Pre booking-Programs query"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Pre booking-Programs query
+            </button>
+            <button
+              value="Pre booking-Advance Booking Window Concern"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Pre booking-Advance Booking Window Concern
+            </button>
+            <button
+              value="Complaints-Complaint during travel"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Complaints-Complaint during travel
+            </button>
+            <button
+              value="Complaints-Complaint After travel"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Complaints-Complaint After travel
+            </button>
+            <button
+              value="Complaints-Complaint supplier"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Complaints-Complaint supplier
+            </button>
+            <button
+              value="Complaints-Complaint booking experience"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Complaints-Complaint booking experience
+            </button>
+            <button
+              value="Complaints-Complaint customer service  experience"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Complaints-Complaint customer service experience
+            </button>
+            <button
+              value="Change and modifications-Change dates"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Change and modifications-Change dates
+            </button>
+            <button
+              value="Change and modifications-Change lead traveler"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Change and modifications-Change lead traveler
+            </button>
+            <button
+              value="Change and modifications-Change room type"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Change and modifications-Change room type
+            </button>
+            <button
+              value=" Change and modifications-Change  Add room "
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Change and modifications-Change Add room
+            </button>
+            <button
+              value=" Change and modifications-Change  Add room "
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Change and modifications-Change Add room
+            </button>
+            <button
+              value="  Change and modifications-Change  Add Activity  "
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Change and modifications-Change Add Activity
+            </button>
+            <button
+              value=" Change and modifications-Change  Add car "
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Change and modifications-Change Add car
+            </button>
+            <button
+              value=" Change and modifications-Change  Add flight "
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Change and modifications-Change Add flight
+            </button>
+            <button
+              value=" Change and modifications-Change  Add travelers "
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Change and modifications-Change Add travelers
+            </button>
+            <button
+              value=" Follow up-Voicemail call back "
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Follow up-Voicemail call back
+            </button>
+            <button
+              value="Follow up-Refund update"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Follow up-Refund update
+            </button>
+            <button value=" IROP" onClick={(e) => DepositionHandleChnage(e)}>
+              IROP
+            </button>
+            <button
+              value=" General Air -Modification Question"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              General Air -Modification Question
+            </button>{" "}
+            <button
+              value=" General Air -Fare rules Question"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              General Air -Fare rules Question
+            </button>{" "}
+            <button
+              value=" General Air -Flexiblity Policies"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              General Air -Flexiblity Policies
+            </button>{" "}
+            <button
+              value=" General Air -confirm flight details"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              General Air -confirm flight details
+            </button>{" "}
+            <button
+              value=" Change and Modifications Air - Voluntary Change Request"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Change and Modifications Air - Voluntary Change Request
+            </button>{" "}
+            <button
+              value="Change and Modifications Air - Schedule Change Involuntary"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Change and Modifications Air - Schedule Change Involuntary
+            </button>{" "}
+            <button
+              value=" Cancel Air-Fully Refundable"
+              onClick={(e) => DepositionHandleChnage(e)}
+            >
+              Cancel Air-Fully Refundable
+            </button>
+           
           </div>
         )}
-          </div>
-          <div style={{height:"30%"}}>
-       <div className={isActive ? "Recordingcontrol" : 'inactive'}>
-          <p>{isActive?"Recording":"Recording Stopped"}</p>
+      </div>
+      <div style={{ height: "30%" }}>
+        <div className={isActive ? "Recordingcontrol" : "inactive"}>
+          <p>{isActive ? "Recording" : "Recording Stopped"}</p>
         </div>
         <div className="controlsbutton">
-        <button className="pauseButton" onClick={pausebuttonfnc}>
+          <button className="pauseButton" onClick={pausebuttonfnc}>
             Pause
           </button>
           <button className="startButton" onClick={startbuttonfnc}>
             Resume
           </button>
         </div>
-    
-      
-
       </div>
-      </div>
-    </>
+    </div>
   );
 }
 
